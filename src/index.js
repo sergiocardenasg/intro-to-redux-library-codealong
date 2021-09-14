035b5d44efcd863260301de95261064708ccb24b
sergiocardenasg <sergiocardenas@utexas.edu>
2021-09-13 21:54:45 -0500

    solved

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux"; /* code change */
 import counterReducer from './reducers/counterReducer.js';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ); /* code change */
+  
+ReactDOM.render(
+    <App store={store} />  /* code change */,
+  document.getElementById("root")
+);
